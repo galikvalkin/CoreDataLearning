a2db43de2fd97d84994f221282889b5c2ffe6311
Valentin <galikvalkin@gmail.com>
2019-06-14 22:40:07 +0300

    fixed issue with keyboard overlapping

--- CoreDataLearning/Controllers/Signin/SigninController.m
+++ CoreDataLearning/Controllers/Signin/SigninController.m
@@ -10,14 +10,69 @@
 
 @interface SigninController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
 @end
 
 @implementation SigninController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.emailTextField.delegate = self;
+    self.passwordTextField.delegate = self;
     self.navigationItem.title = @"Signin";
     NSLog(@"SigninController now used");
+}
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+}
+
+
+- (IBAction)onChangeEmail:(UITextField *)textField {
+    
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    NSLog(@"SignupController viewDidDisappear");
+}
+
+- (BOOL) textFieldShouldReturn:(UITextField *)textField {
+    if (textField == self.emailTextField) {
+        [self.passwordTextField becomeFirstResponder];
+    } else if (textField == self.passwordTextField) {
+        [self.passwordTextField resignFirstResponder];
+        NSLog(@"Thats it folks");
+    }
+    return YES;
+}
+
+- (void)keyboardWillShow:(NSNotification *)notification
+{
+    CGSize keyboardSize = [[[notification userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        CGRect f = self.view.frame;
+        f.origin.y = -50;
+        self.view.frame = f;
+    }];
+}
+
+-(void)keyboardWillHide:(NSNotification *)notification
+{
+    [UIView animateWithDuration:0.3 animations:^{
+        CGRect f = self.view.frame;
+        f.origin.y = 0.0f;
+        self.view.frame = f;
+    }];
 }
 
 @end
